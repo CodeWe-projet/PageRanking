4a6dda7140e5c22df4301a84be5cf38699d3ad3d
allEyezOnCode <alexandredewilde2001@gmail.com>
2022-05-12 18:33:17 +0200

    add second function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # file: src/main.py
-# authors: Theo Technicguy
+# authors: Theo Technicguy, Alexandre Dewilde
 # version: 0.0.1
 #
 # Main file
@@ -20,7 +20,7 @@ with vector_csv.open("r") as f:
 # A Function to calculate the google matrix from P, alpha and v.
 #  G = alpha * P  + (1 - alpha) * e * v.T (where e is a column vector full of 1)
 def compute_g(alpha: float, P :np.array, v: np.array):
-    return alpha * P + (1 - alpha) * (np.ones(len(P)) @ v.T)
+    return alpha * P + (1 - alpha) * np.ones((len(P),)) * v.T
 
 
 # Calculate the transition probabilities matrix from a graph A (an adjacent matrix), p_ij = w_ij / w_i
@@ -41,9 +41,10 @@ def pageRankLinear(A: np.matrix, alpha: float, v: np.array) -> np.array:
     """
     A_b = (np.identity(len(A)) - alpha * p_matrix(A)).T
     b = (1 - alpha) * v
-    return np.linalg.solve(A_b, b)
+    res = np.linalg.solve(A_b, b)
+    return res / sum(res)
 
-from scipy.linalg import eig
+
 def pageRankPower(A: np.matrix, alpha: float, v: np.array) -> np.array:
     """Ex implementation of the PageRank algoritm.
 
@@ -51,5 +52,24 @@ def pageRankPower(A: np.matrix, alpha: float, v: np.array) -> np.array:
     :param alpha: teleportation parameter (0.9) default
     :param v: Personalisation vector
     """
+    P = p_matrix(A)
+    G = compute_g(alpha, P, v)
+    M = G.T
+    
+    # https://en.wikipedia.org/wiki/Power_iteration
 
-    return
+    b_k = np.random.rand(M.shape[1]).reshape(-1, 1)
+
+    for _ in range(30):
+        b_k1 = np.dot(M, b_k)
+        b_k = b_k1 / np.linalg.norm(b_k1)
+    return np.ravel((b_k / sum(b_k)))
+
+import data
+
+# Some debug lines
+matrix = np.matrix(data.data)
+res = pageRankLinear(matrix, 0.9, personnalisation_vector)
+print(res)
+res = (pageRankPower(matrix, 0.9, personnalisation_vector))
+print(res)
